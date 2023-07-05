10fe71de90cd0279a2fc8ce45afd22e46446ae97
vignesh-balaji4535 <127295249+vignesh-balaji4535@users.noreply.github.com>
2023-07-05 15:45:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-/* function printData(data){
-
-    console.log(data)
-
-    let createCard=document.getElementById('card_1');
-
-
-        createCard.innerHTML=`<div class="card text-center">
-        <div class="card-header">
-        Chuck Norris jokes API
-        </div>
-        <img src="${data[0].icon_url}" class="card-img-top" alt="img">
-        <div class="card-body">
-        
-          <h5 class="card-title">${data[0].id}</h5>
-          <p class="card-text">${data[0].updated_at}</p>
-          <p class="card-text">${data[0].value}</p>
-          <a href="${data[0].url}" class="btn btn-primary">Go somewhere</a>
-        </div>
-        <div class="card-footer text-body-secondary">
-          2 days ago
-        </div>
-      </div>`
-    }
-
-
- */
-
-
 
 
 
